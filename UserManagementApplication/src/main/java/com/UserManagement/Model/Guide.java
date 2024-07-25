122df938596d8f282331d1ddc64523c883f5e66c
Soumendu Laha <soumendu.laha.101@gmail.com>
2024-07-25 23:29:38 +0530

    ManyToOne Unidirectional Mapping

--- UserManagementApplication/src/main/java/com/UserManagement/Model/Guide.java
+++ UserManagementApplication/src/main/java/com/UserManagement/Model/Guide.java
@@ -5,29 +5,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
-@Table(name="PhoneNumber",schema = "jpa")
 @Data
-public class PhoneNumber {
-
+@Table(name="guide",schema = "jpa")
+public class Guide {
+	
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long PhId;
+	private Long guideId;
 	
-	@Column(name="number")
-	private String number;
+	@Column(name="guideName")
+	private String guideName;
 	
-	@Column(name="type")
-	private String type;
 	
-	@ManyToOne
-	@JoinColumn(name = "customer_id")
-	private Customer customer;
+
 }
--- UserManagementApplication/src/main/java/com/UserManagement/Model/Student.java
+++ UserManagementApplication/src/main/java/com/UserManagement/Model/Student.java
@@ -1,38 +1,30 @@
-/*
- * package com.UserManagement.Model;
- * 
- * import java.util.List;
- * 
- * import javax.persistence.CascadeType; import javax.persistence.Column; import
- * javax.persistence.Entity; import javax.persistence.GeneratedValue; import
- * javax.persistence.GenerationType; import javax.persistence.Id; import
- * javax.persistence.JoinColumn; import javax.persistence.OneToMany; import
- * javax.persistence.OneToOne; import javax.persistence.Table;
- * 
- * import lombok.Data;
- * 
- * @Entity
- * 
- * @Table(name = "Student", schema = "jpa")
- * 
- * @Data public class Student {
- * 
- * @Id
- * 
- * @GeneratedValue(strategy = GenerationType.IDENTITY) private Long studentId;
- * 
- * @Column(name="StudentName") private String studentName;
- * 
- * // @OneToOne(cascade = CascadeType.ALL)
- * // @JoinColumn(name="studentDetailsId") // private StudentDetails
- * studentDetails; //
- * 
- * @OneToMany(cascade = CascadeType.ALL)
- * 
- * @JoinColumn(name="studentId") private List<Course> listOfCourse;
- * 
- * 
- * 
- * 
- * }
- */
+package com.UserManagement.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="student",schema = "jpa")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long studentId;
+	
+	@Column(name="studentName")
+	private String studentName;
+	
+	@ManyToOne
+	@JoinColumn(name="guideId")
+	private Guide guide;
+
+}
